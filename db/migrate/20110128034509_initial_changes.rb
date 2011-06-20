d6d244de7dab00bc605271ebd9de5b3f1539469f
lee <applesquash@yahoo.com>
2011-06-20 14:43:47 -0700

    bloop

--- db/migrate/20110128034509_initial_changes.rb
+++ db/migrate/20110128034509_initial_changes.rb
@@ -25,7 +25,7 @@ class InitialChanges < ActiveRecord::Migration
     rename_column :survey_options,  :option,    :name
     rename_column :blogs,           :title,     :name
 		
-		remove_column :tester_stats, :users
+		remove_column :users, :tester_stats
     
     add_column    :tickets, :ticket_category_id, :integer
   end
