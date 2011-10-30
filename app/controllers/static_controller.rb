870ccd2562b570d3eca863eac0a3e25f7983f8e3
lee <applesquash@yahoo.com>
2011-10-30 15:14:34 -0700

    all done

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -14,14 +14,16 @@ class StaticController < ApplicationController
       render :overview
     end
 	end
+
 	def contact
-    beta_test = BetaTest.where(:name => 'bettatest.com - help desk')
-    redirect_to beta_test_path(beta_test.id)
 	end
+
 	def unconfirmed_user
 	end
+
 	def unconfirmed_user
 	end
+
   def resend_confirmation
     if current_user && current_user.has_role?(:unconfirmed)
       UserMailer.registration_confirmation(current_user).deliver
