528fdd5d009b69d786af0df676f1051e0548f88a
lee <applesquash@yahoo.com>
2011-10-30 15:09:34 -0700

    all done

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -16,7 +16,7 @@ class StaticController < ApplicationController
 	end
 	def contact
     beta_test = BetaTest.where(:name => 'bettatest.com - help desk')
-    redirect_to beta_test_path(beta_test_id)
+    redirect_to beta_test_path(beta_test.id)
 	end
 	def unconfirmed_user
 	end
