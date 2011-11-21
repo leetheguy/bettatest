66a81e8d1c6c3ef965ca67b36e5c16c46a4f3a2d
lee <applesquash@yahoo.com>
2011-11-21 15:29:39 -0800

    fix email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,6 @@ class UserMailer < ActionMailer::Base
 
   def registration_confirmation(user)
     @user = user
-    binding.pry
     mail(:to => "#{user.name} <#{user.email}>", :subject => "bettatest.com registration confirmation")
   end
 
